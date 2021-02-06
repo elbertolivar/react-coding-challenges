4b1df4db8e9900fa75c9d9aa84cd7808d93cba11
elbertolivar <elbertjames.olivar@gmail.com>
2021-02-06 20:25:58 +0800

    Fetch APIs and load into the state for display

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import getToken from '../helpers/getToken';
+import makeRequest from '../helpers/makeRequest';
 import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
 import '../styles/_discover.scss';
 
@@ -11,6 +13,31 @@ export default class Discover extends Component {
       playlists: [],
       categories: []
     };
+  }
+
+  async componentDidMount() {
+    const token = await getToken();
+    
+    Promise.all([ 
+      this.getNewReleases(token), 
+      this.getPlaylists(token), 
+      this.getCategories(token)
+    ]);
+  }
+
+  async getNewReleases(token) {
+    const { albums: { items: newReleases }} = await makeRequest('new-releases', token);
+    this.setState({ newReleases: newReleases });
+  }
+
+  async getPlaylists(token) {
+    const { playlists: { items: playlists }} = await makeRequest('featured-playlists', token);
+    this.setState({ playlists: playlists });
+  }
+
+  async getCategories(token) {
+    const { categories: { items: categories }} = await makeRequest('categories', token);
+    this.setState({ categories: categories });
   }
 
   render() {
